f279d4f843a947b76aef66d6e071b4f03ceb62fb
novatnova <bilqisshoremekun@gmail.com>
2023-07-16 14:55:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#includes <stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - this is the main function
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
